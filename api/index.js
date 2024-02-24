5c31cd3663af326795db1132a981cfb74e5bac04
Zach Latta <zach@zachlatta.com>
2024-02-24 14:22:55 -0500

    Get basic proxying working

--- api/index.js
+++ api/index.js
@@ -1,8 +1,24 @@
 // for future rate limiting
 import { kv } from '@vercel/kv';
 
-export async function POST(req) {
-  console.log(req.body.model)
+const defaultModel = 'gpt-4-turbo-preview'
+const apiKey = process.env.OPENAI_API_KEY
 
-  return new Response(`Hello from ${req.body.model}`);
+// Proxy
+export async function POST(req) {
+  const path = new URL(req.url).pathname.substring(4) // the .substring removes the leading /api/ from the path, which vercel adds
+  let body = await req.json()
+
+  body.model = defaultModel
+
+  const resp = await fetch(`https://api.openai.com${path}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey} `,
+    },
+    body: JSON.stringify(body)
+  })
+
+  return resp
 }
